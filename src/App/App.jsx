ca8d789d7f42f05b0245860420df7749b7831538
Matthew Hough <matthough1@icloud.com>
2019-03-14 23:42:16 -0400

    update dom + todo list

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { Header } from '../Header';
 import { Session } from '../Session';
 import { Messages } from '../Message';
+import { Send } from '../Send';
 import './App.scss';
+
+/**
+ * Todo for prototype:
+ * 1) style <Send /> & test.
+ * 2) create mutation for <Send /> messages
+ * 3) create subscription for session messages.
+ * 4) add authentication/orization
+ * 5) wrap user in Context provider for children components
+ */
 
 export function App() {
 	return (
@@ -17,6 +27,7 @@ export function App() {
 					/>
 				)}
 			</Session>
+			<Send />
 		</>
 	);
 }
---
+++ src/Send/Send.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Send() {
+	return (
+		<div>
+			<textarea placeholder="type your message here..." />
+			<button>Send</button>
+		</div>
+	);
+}
